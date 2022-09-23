624e5aee7af617cee4552d2439b45097e8efb1e0
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-22 19:37:32 -0700

    created an examples sub directory to showcase usage

---
+++ examples/src/main.rs
@@ -0,0 +1,33 @@
+use std::{fs::File, io::BufReader};
+
+use anyhow::Result;
+use flate2::read::MultiGzDecoder;
+use nom_gtf::GtfReader;
+
+
+fn from_gzip_file(path: &str) -> Result<()> {
+    let count = File::open(path)
+        .map(MultiGzDecoder::new)
+        .map(BufReader::new)
+        .map(GtfReader::from_bufread)?
+        .into_iter()
+        .count();
+    println!("Num records: {}", count);
+    Ok(())
+}
+
+fn from_file(path: &str) -> Result<()> {
+    let count = File::open(path)
+        .map(BufReader::new)
+        .map(GtfReader::from_bufread)?
+        .into_iter()
+        .count();
+    println!("Num records: {}", count);
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    from_file("./data/example.gtf")?;
+    from_gzip_file("./data/example.gtf.gz")?;
+    Ok(())
+}
