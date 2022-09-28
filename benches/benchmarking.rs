d1ed89895612c2d86b5a0b3dc87c8d212d3717c5
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-28 13:56:34 -0700

    added intron benchmarking to benchmarks

--- benches/benchmarking.rs
+++ benches/benchmarking.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use nom_gtf::{GtfRecord, GtfRecordRef};
+use gtftools::{Gtf, GtfRecord, GtfRecordRef};
 
 pub fn benchmark_gtf_record_ref(c: &mut Criterion) {
     let line: &[u8] = br#"1	ensembl_havana	gene	1471765	1497848	.	+	.	gene_id "ENSG00000160072"; gene_version "20"; gene_name "ATAD3B"; gene_source "ensembl_havana"; gene_biotype "protein_coding";"#;
@@ -15,5 +15,12 @@ pub fn benchmark_gtf_record(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, benchmark_gtf_record, benchmark_gtf_record_ref);
+pub fn benchmark_introns(c: &mut Criterion) {
+    let gtf = Gtf::from_file("benches/data/dual.gtf").unwrap();
+    c.bench_function("gtf_introns", |b| {
+        b.iter(|| gtf.introns())
+    });
+}
+
+criterion_group!(benches, benchmark_gtf_record, benchmark_gtf_record_ref, benchmark_introns);
 criterion_main!(benches);
