8597f8754bc7c2f257a81733315a154c8ccc4424
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-07-19 11:12:40 -0700

    update testing

--- src/types/record/record.rs
+++ src/types/record/record.rs
@@ -61,7 +61,7 @@ mod testing {
         let serialized = serde_json::to_string(&record).unwrap();
         assert_eq!(
             serialized,
-            r#"{"seqname":"1","source":"ensembl_havana","feature":"gene","start":1471765,"end":1497848,"score":".","strand":"+","frame":".","gene_id":"ENSG00000160072","gene_version":"20","gene_name":"ATAD3B","transcript_id":null,"transcript_version":null,"transcript_name":null,"transcript_biotype":null,"protein_id":null,"exon_number":null}"#
+            "{\"seqname\":\"1\",\"source\":\"ensembl_havana\",\"feature\":\"gene\",\"start\":1471765,\"end\":1497848,\"score\":\".\",\"strand\":\"+\",\"frame\":\".\",\"ccds_id\":null,\"exon_id\":null,\"exon_number\":null,\"exon_version\":null,\"gene_biotype\":null,\"gene_id\":null,\"gene_name\":\"ATAD3B\",\"gene_source\":\"ensembl_havana\",\"gene_version\":20,\"protein_id\":null,\"protein_version\":null,\"tag\":null,\"transcript_biotype\":null,\"transcript_id\":null,\"transcript_name\":null,\"transcript_source\":null,\"transcript_support_level\":null,\"transcript_version\":null}"
         )
     }
 }
