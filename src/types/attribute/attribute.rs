40f7d328421095fd4ba2dd4c27e38a92fb1bd032
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-07-19 11:13:25 -0700

    added more attributes to the parsing scheme

--- src/types/attribute/attribute.rs
+++ src/types/attribute/attribute.rs
@@ -4,21 +4,34 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct Attribute {
     #[serde(serialize_with = "parse_optional_bytes")]
-    pub gene_id: Option<Vec<u8>>,
+    pub ccds_id: Option<Vec<u8>>,
     #[serde(serialize_with = "parse_optional_bytes")]
-    pub gene_version: Option<Vec<u8>>,
+    pub exon_id: Option<Vec<u8>>,
+    pub exon_number: Option<usize>,
+    pub exon_version: Option<usize>,
+    #[serde(serialize_with = "parse_optional_bytes")]
+    pub gene_biotype: Option<Vec<u8>>,
+    #[serde(serialize_with = "parse_optional_bytes")]
+    pub gene_id: Option<Vec<u8>>,
     #[serde(serialize_with = "parse_optional_bytes")]
     pub gene_name: Option<Vec<u8>>,
     #[serde(serialize_with = "parse_optional_bytes")]
-    pub transcript_id: Option<Vec<u8>>,
+    pub gene_source: Option<Vec<u8>>,
+    pub gene_version: Option<usize>,
     #[serde(serialize_with = "parse_optional_bytes")]
-    pub transcript_version: Option<Vec<u8>>,
+    pub protein_id: Option<Vec<u8>>,
+    pub protein_version: Option<usize>,
     #[serde(serialize_with = "parse_optional_bytes")]
-    pub transcript_name: Option<Vec<u8>>,
+    pub tag: Option<Vec<u8>>,
     #[serde(serialize_with = "parse_optional_bytes")]
     pub transcript_biotype: Option<Vec<u8>>,
     #[serde(serialize_with = "parse_optional_bytes")]
-    pub protein_id: Option<Vec<u8>>,
+    pub transcript_id: Option<Vec<u8>>,
     #[serde(serialize_with = "parse_optional_bytes")]
-    pub exon_number: Option<Vec<u8>>,
+    pub transcript_name: Option<Vec<u8>>,
+    #[serde(serialize_with = "parse_optional_bytes")]
+    pub transcript_source: Option<Vec<u8>>,
+    #[serde(serialize_with = "parse_optional_bytes")]
+    pub transcript_support_level: Option<Vec<u8>>,
+    pub transcript_version: Option<usize>,
 }
